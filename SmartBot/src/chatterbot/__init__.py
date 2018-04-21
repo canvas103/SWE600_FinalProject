c52be6e8a7499bfc3d266da7674d223baf16844e
Alfred Yuan <yuanzz214@gmail.com>
2018-04-21 15:35:12 -0700

    refactoring

--- SmartBot/src/chatterbot/__init__.py
+++ SmartBot/src/chatterbot/__init__.py
@@ -1,9 +1,6 @@
-"""
-ChatterBot is a machine learning, conversational dialog engine.
-"""
 from .chatterbot import ChatBot
 
-__version__ = '0.8.5'
+__version__ = '0.0.1'
 
 __all__ = (
     'ChatBot',
--- SmartBot/src/chatterbot/constants.py
+++ SmartBot/src/chatterbot/constants.py
@@ -1,12 +1,3 @@
-"""
-ChatterBot constants
-"""
-
-'''
-The maximum length of characters that the text of a statement can contain.
-This should be enforced on a per-model basis by the data model for each
-storage adapter.
-'''
 STATEMENT_TEXT_MAX_LENGTH = 400
 
 # The maximum length of characters that the name of a tag can contain
